5553b225bc1737eb5846be53f435a569516b3bf0
Daniel Fireman <danielfireman@gmail.com>
2022-07-13 14:54:24 +0000

    Tratando requests com operando string

--- api/src/test/java/com/danielfireman/ifal/calcapi/RaizTest.java
+++ api/src/test/java/com/danielfireman/ifal/calcapi/RaizTest.java
@@ -1,11 +1,13 @@
 package com.danielfireman.ifal.calcapi;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 import io.jooby.MockRouter;
 import io.jooby.StatusCode;
+import io.jooby.exception.BadRequestException;
 
 public class RaizTest {
     @Test
@@ -14,6 +16,14 @@ public class RaizTest {
         router.get("/raiz/16", rsp -> {
             assertEquals(4.0, rsp.value());
             assertEquals(StatusCode.OK, rsp.getStatusCode());
+        });
+    }
+
+    @Test
+    public void raiz_operadorString() {
+        MockRouter router = new MockRouter(new App());
+        assertThrows(BadRequestException.class, () ->{
+            router.get("/raiz/aa", rsp -> {});
         });
     }
 }
