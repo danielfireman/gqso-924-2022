9475ef24febd92e2ba56b5fa80e4b11709f50ee6
Daniel Fireman <danielfireman@gmail.com>
2022-09-28 13:43:45 +0000

    corrige erro

--- api/src/main/java/com/danielfireman/ifal/calcapi/Raiz.java
+++ api/src/main/java/com/danielfireman/ifal/calcapi/Raiz.java
@@ -11,7 +11,7 @@ public class Raiz {
     @GET
     public double calculaRaiz(@PathParam("op") String op) {
         try {
-            return Double.parseDouble(op);
+            return Math.sqrt(Double.parseDouble(op));
         } catch (NumberFormatException nfe) {
             throw new BadRequestException("Parâmetro inválido: " + op);
         }
